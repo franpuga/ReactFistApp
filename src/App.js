a226f7a5457859c5cd8b2d8109f9ab28f5fc363d
Francisco Javier Puga Castro <fjpuga@aldaba.es>
2019-01-04 10:24:36 +0100

    GitHub API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+const Card = (props) => {
+	return (
+  	<div style={{margin: '1em'}}>
+    	<img width="75" src={props.avatar_url} alt="Foto Perfil USuario"/>
+    	<div style={{display:'inline-block', marginLeft:10}}>
+    		<div style={{fontSize:'1.25em', fontWeight:'bold'}}>{props.name}</div>
+      	<div>{props.company}</div>
+    	</div>
+    </div>
+  );
+};
+
+const CardList = (props) => {
+	return (
+  	<div>
+  	  {props.cards.map(card => <Card key ={card.id} {...card}/>)}
+  	</div>
+  );
+}
+
+class Form extends Component{
+	state = {userName: ''}
+	handleSubmit = (event) => {
+  	event.preventDefault();
+    axios.get(`https://api.github.com/users/${this.state.userName}`)
+    	.then(resp => {
+      	this.props.onSubmit(resp.data);
+        this.setState({userName: ''});
+      });
+  };
+	render(){
+  return(
+  	<form className="container" onSubmit={this.handleSubmit}>
+    	<input className="form-control" type="text" value ={this.state.userName} onChange={(event) => this.setState({userName: event.target.value})} placeholder="Nombre Usuario GitHub" required />
+      <button className="btn btn-primary" type="submit"> Añadir Usuario</button>
+    </form>
+  );
+  }
+}
+
+class App extends React.Component{
+	state = {
+  	cards : []
+  };
+  
+  addNewCard = (cardInfo) => {
+  	this.setState(prevState => ({
+    cards: prevState.cards.concat(cardInfo)
+    }));
+  };
+  
+	render(){
+  	return(
+    	<div>
+      	<Form onSubmit={this.addNewCard}/>
+        <CardList cards={this.state.cards}/>
       </div>
     );
   }
